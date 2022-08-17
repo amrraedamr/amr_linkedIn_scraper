1912329609024d3856e026a72588aee5e0cb66c6
Amr Amr <67161059+amrraedamr@users.noreply.github.com>
2022-08-17 17:27:54 +0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -2,12 +2,12 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from Scraper import Scraper
-import json
+# import json
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 email = "ztataz04@gmail.com"
 password = "DR#(Li,g#2Q.F,Z"
-linkedin_url = 'https://www.linkedin.com/in/sandraaclark/'
+linkedin_url = 'https://www.linkedin.com/in/denniskoutoudis/'
 
 obj = Scraper(driver, email, password)
 obj.scrape_profile(linkedin_url)
@@ -15,8 +15,11 @@ data = obj.get_data()
 
 # [] = for i obj.scrape_profile([i]).data
 
-data_json = json.dumps(data)
+# data_json = json.dumps(data)
 
 driver.quit()
 
-print(data_json)
+# print(data_json)
+
+for key, value in data.items():
+    print(key, ': ', value)
